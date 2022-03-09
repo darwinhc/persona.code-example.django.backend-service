3e2e50563c02e81703591e6fdcbdacb66e123877
Darwin Herrera C <darwinsherrerac@gmail.com>
2022-03-09 17:10:58 -0500

    Heroku settings

--- applications/persona/views.py
+++ applications/persona/views.py
@@ -6,11 +6,6 @@ from rest_framework import viewsets
 from .serializers import PersonSerializer
 from .models import Persona
 
-class ListaPersonas(ListAPIView):
-    serializer_class = PersonSerializer
-    queryset = Persona.objects.all()
-
-
 class PersonaCRUD(viewsets.ModelViewSet):
     serializer_class = PersonSerializer
     queryset = Persona.objects.all()
--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -22,3 +22,5 @@ DATABASES = {
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
